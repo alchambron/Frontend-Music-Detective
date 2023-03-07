4a4761b9639a52399f0cd8b2c7ca86abaf48d257
Thomas Raducha <thomasraducha@gmail.com>
2023-03-07 10:46:43 +0100

    Adding page Game

---
+++ src/pages/Game/Game.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactPlayer from '../../components/ReactPlayer/Reactplayer'
+
+export default function Game() {
+    return (
+        <div className='game'>
+            <ReactPlayer />
+        </div>
+    )
+}
