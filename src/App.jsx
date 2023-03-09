ff31eedcf2eb87fa68130b459c1dd7cccb8cd75d
Thomas Raducha <thomasraducha@gmail.com>
2023-03-09 15:28:33 +0100

    adding routes for results page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ChoicePlaylist from './pages/ChoicePlaylist'
 
 import Game from './pages/Game'
 import Home from './pages/Home'
+import Results from './pages/Results'
 
 export default function App() {
 
@@ -12,7 +13,9 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/game/:id" element={<Game />} />
         <Route path="/choice" element={<ChoicePlaylist />} />
-     
+        <Route path="/results/:id" element={<Results />} />
+
+
       </Routes>
     </div>
   )
