b8fb67a8b4c046382294cc83b26d56941c25eaf7
Thomas Raducha <thomasraducha@gmail.com>
2023-03-13 17:34:33 +0100

    adding button sign up and login in page my account

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function MyAccount() {
     return (
         <div>
             <h1>Mon compte</h1>
+            <NavLink to="/register">
+                <button>Sign up</button>
+            </NavLink>
+            <NavLink to="/login">
+                <button>Login</button>
+            </NavLink>
         </div>
     )
 }
