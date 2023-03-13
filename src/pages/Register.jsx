70d2a9ee6bfa3b6123c6fe80079627eb0ba84166
Thomas Raducha <thomasraducha@gmail.com>
2023-03-13 15:25:50 +0100

    fix_bug_register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,45 +1,42 @@
-import React, { useState } from 'react';
-import useFetch from '../services/useFetch';
-import Cookies from 'js-cookie';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState } from "react";
+import useFetch from '../services/useFetch'
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 export default function Register() {
-    const [formData, setFormData] = useState({
-        email: "",
-        password: "",
+    const [form, setForm] = useState({
+        user: {
+            email: " ",
+            password: " ",
+        },
     });
-
     const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    function handleChange(e) {
+        setForm({
+            ...form,
+            user: {
+                ...form.user,
+                [e.target.id]: e.target.value,
+            },
+        });
+    }
 
+    async function handleSubmit(e) {
+        e.preventDefault();
         const sendData = {
             method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-type": "application/json",
             },
-            body: JSON.stringify(formData)
-        }
-        const apiURL = 'https://musicdetective.herokuapp.com/users';
-        try {
-            const data = await useFetch({ apiURL }, sendData);
-            const token = data.token;
+            body: JSON.stringify(form),
+        };
 
-            Cookies.set("user_token", token);
-            navigate("/");
-        } catch (error) {
-            console.log(error.message);
-            alert("An error occurred during the connection.");
-        }
-    }
+        const data = await useFetch("https://musicdetective.herokuapp.com/users/", sendData);
+        const token = data.token;
 
-    const handleChange = (e) => {
-        setFormData({
-            ...formData.user,
-            [e.target.id]: e.target.value,
-        });
+        Cookies.set("user_token", token);
+        navigate("/");
     }
 
     return (
@@ -47,12 +44,14 @@ export default function Register() {
             <label htmlFor="email">Email:</label>
             <input
                 type="email"
+                name=""
                 id="email"
                 onChange={handleChange} />
 
             <label htmlFor="password">Password:</label>
             <input
                 type="password"
+                name=""
                 id="password"
                 onChange={handleChange} />
 
--- src/services/useFetch.js
+++ src/services/useFetch.js
@@ -1,5 +1,6 @@
-export default async function useFetch(url, settings) {
-  const response = await fetch(url, settings);
+export default async function useFetch(url, parameters) {
+  const response = await fetch(url, parameters);
+  const token = await response.headers.get("Authorization")
   const data = await response.json();
-  return data;
+  return { data, token };
 }
