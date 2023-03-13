0fbb878356cb8056d602de2a82fdc9d4060029e4
Thomas Raducha <thomasraducha@gmail.com>
2023-03-13 10:44:48 +0100

    Adding routes for signup

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ChoicePlaylist from './pages/ChoicePlaylist'
 import Game from './pages/Game'
 import Home from './pages/Home'
 import Results from './pages/Results'
+import SignUpForm from './pages/SignUpForm'
 
 export default function App() {
 
@@ -14,8 +15,7 @@ export default function App() {
         <Route path="/game/:id" element={<Game />} />
         <Route path="/choice" element={<ChoicePlaylist />} />
         <Route path="/results/:id" element={<Results />} />
-
-
+        <Route path="/signup" element={<SignUpForm />} />
       </Routes>
     </div>
   )
