32fe144b374ad39f4239c0b703174f1ba961bd76
Thomas Raducha <thomasraducha@gmail.com>
2023-03-13 11:05:33 +0100

    Updated Fetch and Cookies

--- src/pages/SignUpForm.jsx
+++ src/pages/SignUpForm.jsx
@@ -1,21 +1,50 @@
-import React from 'react'
-
-
+import React, { useState } from 'react'
+import useFetch from '../services/useFetch'
 export default function SignUpForm() {
+    const [formData, setFormData] = useState({
+        email: " ",
+        password: " ",
+    });
+    console.log("🚀 ~ file: SignUpForm.jsx:9 ~ SignUpForm ~ formData:", formData)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const sendData = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        }
+        const apiURL = 'https://musicdetective.herokuapp.com/';
+        const data = await useFetch({ apiURL }, sendData)
+        const token = await data.token
+
+        Cookies.set("user_token", token)
+        navigate("/");
+
+    }
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+    }
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <label htmlFor="email">Email:</label>
             <input
                 type="email"
                 id="email"
-            />
+                onChange={handleChange} />
 
             <label htmlFor="password">Password:</label>
             <input
                 type="password"
                 id="password"
-            />
+                onChange={handleChange} />
 
             <button type="submit">Login</button>
         </form>
