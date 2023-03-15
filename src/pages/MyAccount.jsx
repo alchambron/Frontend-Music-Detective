96dcc07625113a6fdc4a74e17e4b6691a2ed49e4
Thomas Raducha <thomasraducha@gmail.com>
2023-03-15 11:30:32 +0100

    adding nickname

--- src/pages/MyAccount.jsx
+++ src/pages/MyAccount.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import Cookies from 'js-cookie'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 
+
 export default function MyAccount() {
+    const [nickname, setNickname] = useState("");
+    const token = Cookies.get("user_token");
+
+    const fetchData = async () => {
+        const params = {
+            headers: {
+                Authorization: `${token}`
+            }
+        }
+        const response = await fetch("https://musicdetective.herokuapp.com/member-data", params)
+        const data = await response.json();
+        setNickname(data.user.nickname);
+    }
+
+    useEffect(() => {
+        fetchData();
+
+    }, [])
+
     return (
         <div>
             <h1>Mon compte</h1>
+            <p>Nickname: {nickname}</p>
             <NavLink to="/register">
                 <button>Sign up</button>
             </NavLink>
@@ -14,3 +36,4 @@ export default function MyAccount() {
         </div>
     )
 }
+
