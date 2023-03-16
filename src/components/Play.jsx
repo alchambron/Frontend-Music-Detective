75de12260f642b9daa0187c29f656caf94c3698e
alchambron <alexis.chambron@gmail.com>
2023-03-16 16:00:19 +0100

    Refactoring HTML of Play

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -3,7 +3,7 @@ import ReactPlayer from "react-player";
 import Compare from "../services/Compare";
 import Countdown from "./Counter";
 import Vinyl from "./Vinyl";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 export default function Play({
   searchResults,
@@ -11,12 +11,14 @@ export default function Play({
   activateSearchBar,
 }) {
   const [display, setDisplay] = useState(false);
+  const [displaySuccess, setDisplaySuccess] = useState(false);
+  const [displayDanger, setDisplayDanger] = useState(false);
   const [selectSong, setSelectSong] = useState(null);
   const [currentSong, setCurrentSong] = useState(null);
   const [progress, setProgress] = useState(0);
   const [userChoice, setUserChoice] = useState("");
   const [matchingResults, setMatchingResults] = useState(false);
-  const [volume, setVolume] = useState(0.5);
+  const [volume, setVolume] = useState(0);
   const [points, setPoints] = useState(100);
   const [giveUp, setGiveUp] = useState(false);
   const [songFinished, setSongFinished] = useState(false);
@@ -34,6 +36,17 @@ export default function Play({
       setMatchingResults(matchingResults);
       manageSearchBar(true);
       console.log(matchingResults);
+      if (!matchingResults) {
+        setDisplayDanger(true);
+        setTimeout(() => {
+          setDisplayDanger(false);
+        }, 3000);
+      } else {
+        setDisplaySuccess(true);
+        setTimeout(() => {
+          setDisplaySuccess(false);
+        }, 5000);
+      }
     }
   }, [searchResults]);
 
@@ -41,7 +54,6 @@ export default function Play({
     if (matchingResults) {
       stopPlayer();
       addPoints();
-      alert("Attention son suivant !");
 
       setTimeout(() => {
         NextSong();
@@ -72,7 +84,9 @@ export default function Play({
     console.log("choose");
     try {
       const playlistNumber = window.location.pathname.match(/\/(\d+)$/)[1];
-      const apiURL = import.meta.env.VITE_BASE_URL + `/playlist_contents?playlist_id=${playlistNumber}`;
+      const apiURL =
+        import.meta.env.VITE_BASE_URL +
+        `/playlist_contents?playlist_id=${playlistNumber}`;
       const response = await fetch(apiURL);
       const data = await response.json();
       const filteredData = data.filter(
@@ -163,19 +177,14 @@ export default function Play({
       NextSong();
     }, 5000);
   }
-  function handleLeave() {
-    stopPlayer();
-    navigate("../choice");
-  }
 
   return (
     <div className="play">
       <Countdown time={3} onFinish={handleCountdownFinish} />
       {display && (
-        <div>
-          <Vinyl />
+        <>
           {currentSong && (
-            <div key={currentSong.youtube_id}>
+            <div className="player" key={currentSong.youtube_id}>
               <ReactPlayer
                 url={`https://www.youtube.com/watch?v=${currentSong.youtube_id}`}
                 playing={true}
@@ -187,33 +196,69 @@ export default function Play({
               />
             </div>
           )}
-          <div>
-            <input
-              type="range"
-              min="0"
-              max="1"
-              step="0.01"
-              value={volume}
-              onChange={handleVolumeChange}
-            />
-            <progress value={progress} max="20" />
+          <div className="leave">
+            <NavLink to="/choice">
+              <p>Retour</p>
+            </NavLink>
           </div>
-          <>
-            {songFinished && <button onClick={handleReplay}>Replay</button>}
+          <div className="play__display">
+            <div className="play__display__infos">
+              <p className="play__display__infos__points">
+                Score : {points} points
+              </p>
+              {giveUp && (
+                <p className="play__display__infos__answer">
+                  Le titre de la musique est: {selectSong.youtube_title}
+                </p>
+              )}
+            </div>
+            <div className="play__display__elements">
+              <h3>Musique en cours</h3>
+              <progress
+                className="play__display__elements__progress"
+                value={progress}
+                max="20"
+              />
+            </div>
+            <div className="play__display__interactive">
+              {displayDanger && (
+                <div className="play__display__interactive__danger">
+                  <p>Mauvaise réponse : Recommencer</p>
+                </div>
+              )}
+              {displaySuccess && (
+                <div className="play__display__interactive__success">
+                  <p>Felicitation !</p>
+                </div>
+              )}
+            </div>
+            <div className="play__display__volume">
+              <h6>Volume</h6>
+              <input
+                className="play__display__volume__bar"
+                type="range"
+                min="0"
+                max="1"
+                step="0.01"
+                value={volume}
+                onChange={handleVolumeChange}
+              />
+            </div>
+          </div>
+          <div className="play__buttons">
             {songFinished && (
-              <button onClick={handleAbandon}>REPONCE</button>
+              <button className="button-play" onClick={handleReplay}>
+                Réécouter
+              </button>
             )}
-
-            <button onClick={stopPlayer}>STOP</button>
-
-            <button onClick={handleLeave}>RETOUR PLAYLIST</button>
-            {matchingResults && <p>Felicitation</p>}
-            {giveUp && (
-              <p>Le titre de la musique est: {selectSong.youtube_title}</p>
+            {songFinished && (
+              <button className="button-play" onClick={handleAbandon}>
+                Réponse
+              </button>
             )}
-            <p>Vous avez: {points} points/100</p>
-          </>
-        </div>
+            {/* <button className="button-play" onClick={stopPlayer}>Stop</button> */}
+          </div>
+        </>
       )}
     </div>
   );
