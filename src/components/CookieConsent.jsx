488af2e84c20ce46fba55b0a5155b4abf97f385e
thimibka <fseetp@gmail.com>
2023-03-16 20:53:04 +0100

    cookie consent

---
+++ src/components/CookieConsent.jsx
@@ -0,0 +1,31 @@
+import CookieConsent from "react-cookie-consent";
+import { NavLink } from "react-router-dom";
+
+
+export default function () {
+
+
+
+  return (
+    <CookieConsent
+      debug={true}
+      location="bottom"
+      style={{ background: "#111D4A", textAligne: "left", color: "#EFEFD0" }}
+      buttonStyle={{
+        color: "#111D4A",
+        background: "#EFEFD0",
+        fontSize: "14px",
+        borderRadius: "25px",
+      }}
+      buttonText="Accepter"
+      expires={365}
+    >
+      Ce site utilise des cookies. Pour en savoir plus,cliquer sur 
+      <NavLink style={{ color: '#FFFFFF' }} to="../Privacy" className="buttonPrivacy" >   CONSULTEZ  </NavLink>
+
+      notre politique de confidentialité.
+    </CookieConsent>
+  );
+}
+
+
