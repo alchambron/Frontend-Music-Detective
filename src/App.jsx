a10192f79812c98c042e5b0a00a6becc71d8dddc
Thomas Raducha <thomasraducha@gmail.com>
2023-03-17 11:49:20 +0100

    adding UidContext.Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,43 @@
 import { Routes, Route } from 'react-router-dom'
+import { UidContext } from './components/AppContext'
+import { useEffect, useState } from 'react'
+import Cookies from 'js-cookie'
+
 import Admin from './pages/Admin'
 import ChoicePlaylist from './pages/ChoicePlaylist'
 import EditAccount from './pages/EditAccount'
 import Game from './pages/Game'
 import Home from './pages/Home'
-import MyAccount from './pages/MyAccount'
 import Results from './pages/Results'
+import Profile from './pages/Profile'
 
 export default function App() {
+  const [uid, setUid] = useState(null);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      const token = Cookies.get("user_token");
+      console.log("🚀 ~ file: App.jsx:20 ~ fetchToken ~ token:", token)
+      if (token !== null && token !== undefined) {
+        setUid(token)
+      }
+    }
+    fetchToken();
+  }, [uid])
 
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/admin' element={<Admin />} />
-        <Route path="/game/:id" element={<Game />} />
-        <Route path="/choice" element={<ChoicePlaylist />} />
-        <Route path="/results/:id" element={<Results />} />
-        <Route path="/myaccount" element={<MyAccount />} />
-        <Route path="/edit" element={<EditAccount />} />
-
-      </Routes>
+      <UidContext.Provider value={uid}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/admin' element={<Admin />} />
+          <Route path="/game/:id" element={<Game />} />
+          <Route path="/choice" element={<ChoicePlaylist />} />
+          <Route path="/results/:id" element={<Results />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/edit" element={<EditAccount />} />
+        </Routes>
+      </UidContext.Provider>
     </div>
   )
 }
