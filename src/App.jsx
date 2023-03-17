2da79bc246940cc78ddc57085a4e8f56de590e60
Thomas Raducha <thomasraducha@gmail.com>
2023-03-17 10:48:53 +0100

    delete routes for Register and Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ import { Routes, Route } from 'react-router-dom'
 import Admin from './pages/Admin'
 import ChoicePlaylist from './pages/ChoicePlaylist'
 import EditAccount from './pages/EditAccount'
-
 import Game from './pages/Game'
 import Home from './pages/Home'
-import Login from './pages/Login'
 import MyAccount from './pages/MyAccount'
-import Register from './pages/Register'
 import Results from './pages/Results'
 
 export default function App() {
@@ -20,8 +17,6 @@ export default function App() {
         <Route path="/game/:id" element={<Game />} />
         <Route path="/choice" element={<ChoicePlaylist />} />
         <Route path="/results/:id" element={<Results />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/myaccount" element={<MyAccount />} />
         <Route path="/edit" element={<EditAccount />} />
 
