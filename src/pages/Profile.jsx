71fb86a6c7107df18065eabcc49c64bfd4391503
Thomas Raducha <thomasraducha@gmail.com>
2023-03-17 15:21:12 +0100

    refactor fetchData

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,26 +1,35 @@
 import Cookies from 'js-cookie'
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom'
+import { logoutUser } from '../actions/userAction';
 import Sign from '../components/Log/Sign';
 
 export default function Profile() {
     const [nickname, setNickname] = useState("");
+    const [userLoggedIn, setUserLoggedIn] = useState(false);
     const token = Cookies.get("user_token");
+    const dispatch = useDispatch();
+    const loggedUser = useSelector((state) => {
+        return state.user
+    })
+    useEffect(() => {
+        if (loggedUser?.id) {
+            setUserLoggedIn(true);
+        } else {
+            setUserLoggedIn(false)
+        }
+    }, [loggedUser]);
 
     const handleClickLogOut = () => {
         Cookies.remove('user_token');
+        dispatch(logoutUser())
     }
 
     const fetchData = async () => {
-        const params = {
-            headers: {
-                Authorization: `${token}`
-            }
-        }
         try {
-            const response = await fetch(import.meta.env.VITE_BASE_URL + "/member-data", params)
-            const data = await response.json();
-            setNickname(data.user.nickname);
+            const user = await getUserProfile();
+            setNickname(user.nickname);
         } catch (error) {
             console.error(error);
         }
@@ -49,7 +58,7 @@ export default function Profile() {
     return (
         <div>
             <h1>Mon compte</h1>
-            {token == null || token == undefined ? (
+            {!userLoggedIn ? (
                 <Sign />
             ) : (
                 <>
