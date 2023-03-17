ee1070a6f726ffef36bb168ca267056388c7febc
Thomas Raducha <thomasraducha@gmail.com>
2023-03-17 11:49:46 +0100

    rename Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import Sign from '../components/Log/Sign';
 
-export default function MyAccount() {
+export default function Profile() {
     const [nickname, setNickname] = useState("");
     const token = Cookies.get("user_token");
 
