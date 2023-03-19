3ef83add0525e1bf5689f0ab52f5698042d8fa58
Thomas Raducha <thomasraducha@gmail.com>
2023-03-19 21:27:15 +0100

    create component delete profil

---
+++ src/components/Profile/DeleteProfil.jsx
@@ -0,0 +1,26 @@
+import Cookies from 'js-cookie';
+import React from 'react'
+
+export default function DeleteProfile({ handleClickLogOut }) {
+    const token = Cookies.get("user_token");
+
+    async function handleClickDeleteAccount(e) {
+        const params = {
+            method: "DELETE",
+            headers: {
+                Authorization: `${token}`,
+            },
+        };
+        try {
+            await fetch(import.meta.env.VITE_BASE_URL + "/users", params);
+            handleClickLogOut()
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return (
+        <div>
+            <button onClick={handleClickDeleteAccount}>Delete my account</button>
+        </div>
+    )
+}
