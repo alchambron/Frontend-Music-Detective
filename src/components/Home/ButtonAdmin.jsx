7ac9b8f381418b8dcd77d5685a92eda9316af5e2
Thomas Raducha <thomasraducha@gmail.com>
2023-03-20 17:33:47 +0100

    Create component ButtonAdmin

---
+++ src/components/Home/ButtonAdmin.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function ButtonAdmin() {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate("/admin")
+    }
+    return (
+        <div>
+            <button onClick={handleClick}>Page Admin</button>
+        </div>
+    )
+}
