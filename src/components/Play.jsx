b1a2d34ebcb19f98e9cdafed0abda4fba6462bf8
Thomas Raducha <thomasraducha@gmail.com>
2023-03-20 21:48:14 +0100

    Refactoring PlayInfo

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -9,6 +9,7 @@ import ProgressBar from "./Play/ProgressBar";
 import InteractiveResponse from "./Play/InteractiveResponse";
 import ButtonReturn from "./Play/ButtonReturn";
 import PlayButtons from "./Play/PlayButtons";
+import PlayInfo from "./Play/PlayInfo";
 
 export default function Play({
   searchResults,
@@ -167,7 +168,10 @@ export default function Play({
 
   return (
     <div className="play">
-      <Countdown time={3} onFinish={handleCountdownFinish} />
+      <Countdown
+        time={3}
+        onFinish={handleCountdownFinish}
+      />
       {display && (
         <>
           {currentSong && (
@@ -180,19 +184,12 @@ export default function Play({
           )}
           <ButtonReturn />
           <div className="play__display">
-            <div className="play__display__infos">
-              <p className="play__display__infos__points">
-                Score : {score} points
-              </p>
-              {giveUp && (
-                <p className="play__display__infos__answer">
-                  Le titre de la musique est: {selectSong.youtube_title}
-                </p>
-              )}
-            </div>
-            <ProgressBar
-              progress={progress}
+            <PlayInfo
+              selectSong={selectSong}
+              score={score}
+              giveUp={giveUp}
             />
+            <ProgressBar progress={progress} />
             <InteractiveResponse
               displayDanger={displayDanger}
               displaySuccess={displaySuccess}
