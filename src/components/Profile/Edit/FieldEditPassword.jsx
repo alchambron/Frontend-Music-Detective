d22617f246cd4484fb74799b52be23d04bc2db01
Thomas Raducha <thomasraducha@gmail.com>
2023-03-23 10:43:19 +0100

    create component FieldEditPassword

---
+++ src/components/Profile/Edit/FieldEditPassword.jsx
@@ -0,0 +1,12 @@
+// @flow 
+import * as React from 'react';
+type Props = {
+    
+};
+export const FieldEditPassword = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
