48640f53fc32ebc500ddeab2b81a2a253bfb279a
Thomas Raducha <thomasraducha@gmail.com>
2023-03-23 10:42:40 +0100

    create component FieldEdit

---
+++ src/components/Profile/Edit/FieldEdit.jsx
@@ -0,0 +1,12 @@
+// @flow 
+import * as React from 'react';
+type Props = {
+    
+};
+export const FieldEdit = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
