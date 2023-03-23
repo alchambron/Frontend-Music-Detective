1cb0a5a0c78d4908136c9f6ffe44ac94c0e2068f
Thomas Raducha <thomasraducha@gmail.com>
2023-03-23 13:30:39 +0100

    refactoring to child component

--- src/components/Profile/Edit/FieldEditEmail.jsx
+++ src/components/Profile/Edit/FieldEditEmail.jsx
@@ -1,12 +1,25 @@
-// @flow 
-import * as React from 'react';
-type Props = {
-    
-};
-export const FieldEditEmail = (props: Props) => {
+import { useState } from "react";
+import InputField from "../../InputField";
+
+export const FieldEditEmail = ({ onEmailChange, formData }) => {
+    const [email, setEmail] = useState(formData.email);
+
+    const handleEmailChange = (event) => {
+        const newEmail = event.target.value;
+        setEmail(newEmail);
+        onEmailChange({ email: newEmail });
+    };
+
     return (
-        <div>
-            
+        <div className="edit__form__user__infos">
+            <InputField
+                label="Adresse mail"
+                type="email"
+                id="email"
+                name="email"
+                value={email}
+                onChange={handleEmailChange}
+            />
         </div>
     );
 };
--- src/components/Profile/Edit/FieldEditNickname.jsx
+++ src/components/Profile/Edit/FieldEditNickname.jsx
@@ -1,12 +1,25 @@
-// @flow 
-import * as React from 'react';
-type Props = {
-    
-};
-export const FieldEditNickname = (props: Props) => {
+import { useState } from "react";
+import InputField from "../../InputField";
+
+export const FieldEditNickname = ({ onNicknameChange, formData }) => {
+    const [nickname, setNickname] = useState(formData.nickname);
+
+    const handleNicknameChange = (event) => {
+        const newNickname = event.target.value;
+        setNickname(newNickname);
+        onNicknameChange({ nickname: newNickname });
+    };
+
     return (
-        <div>
-            
+        <div className="edit__form__user__infos">
+            <InputField
+                label="Pseudo"
+                type="text"
+                id="nickname"
+                name="nickname"
+                value={nickname}
+                onChange={handleNicknameChange}
+            />
         </div>
     );
 };
