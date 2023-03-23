6071d381a3404fa3e4bda45974b6c9095ad304d3
Thomas Raducha <thomasraducha@gmail.com>
2023-03-23 13:28:37 +0100

    create model

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,9 +1,18 @@
 export default function InputField({ label, type, id, value, onChange }) {
 
     return (
-        <div className="edit__form__user__infos">
-            <label htmlFor={id}>{label}</label>
-            <input type={type} id={id} name={id} value={value} onChange={onChange} />
-        </div>
+        <>
+            <label
+                htmlFor={id}>{label}
+            </label>
+
+            <input
+                type={type}
+                id={id}
+                name={id}
+                value={value}
+                onChange={onChange}
+            />
+        </>
     );
 }
