11881d2200e285aa6f21528aa2a1cd45d23fa931
Thomas Raducha <thomasraducha@gmail.com>
2023-03-23 10:42:55 +0100

    create component FieldEditMail

---
+++ src/components/Profile/Edit/FieldEditEmail.jsx
@@ -0,0 +1,12 @@
+// @flow 
+import * as React from 'react';
+type Props = {
+    
+};
+export const FieldEditEmail = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
